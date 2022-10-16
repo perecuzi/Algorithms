a7bb78baffc3ec2951c4cac0759b0a2a03cf009d
Edi Peremobowei <egistryanedi@gmail.com>
2022-10-16 13:02:02 +0200

    Maximum Pairwise Product

---
+++ max_pairwise_product.py
@@ -0,0 +1,1 @@
+def max_pairwise_product(numbers):    n = len(numbers)    numbers.sort()    return numbers[n-2] * numbers[n-1]if __name__ == '__main__':    input_n = int(input())    input_numbers = [int(x) for x in input().split()]    print(max_pairwise_product(input_numbers))
